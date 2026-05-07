0a97eabd108679a7d215bee4e4903939b194e47f
heynendo <heynen.donovan@gmail.com>
2026-05-06 21:18:55 -0500

    Update to include Typescript, TailwindCSS, and more to template.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom"
 import Nav from "./Nav"
 
 export default function Layout() {
-    return (
-        <>
-            <Nav />
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  )
 }
